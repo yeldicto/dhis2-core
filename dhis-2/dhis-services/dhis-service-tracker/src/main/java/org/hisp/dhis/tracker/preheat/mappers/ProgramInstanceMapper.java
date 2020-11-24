b1090e1995fef897dc90beaee4bee1aa460b7193
Luciano Fiandesio <luciano@fiandes.io>
2020-11-24 17:32:01 +0100

    removed fields from mapping for better performance

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/mappers/ProgramInstanceMapper.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/tracker/preheat/mappers/ProgramInstanceMapper.java
@@ -1,13 +1,22 @@
 package org.hisp.dhis.tracker.preheat.mappers;
 
 import org.hisp.dhis.program.ProgramInstance;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = DebugMapper.class)
+@Mapper( uses = DebugMapper.class )
 public interface ProgramInstanceMapper extends PreheatMapper<ProgramInstance>
 {
     ProgramInstanceMapper INSTANCE = Mappers.getMapper( ProgramInstanceMapper.class );
+
+    @Mapping( target = "messageConversations", ignore = true )
+    @Mapping( target = "relationshipItems", ignore = true )
+    @Mapping( target = "translations", ignore = true )
+    @Mapping( target = "favorites", ignore = true )
+    @Mapping( target = "href", ignore = true )
+    @Mapping( target = "programStageInstances", ignore = true )
 
     ProgramInstance map( ProgramInstance programInstance );
 }
